133481f78bc4cf0dc7fa84be471c219c76309937
Maciej Czarnecki <mcczarny@gmail.com>
2022-10-02 11:22:30 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { TestView } from './issueView';
+import * as logAnalyser from './logAnalyser';
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "customisable-log-analyser" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('customisable-log-analyser.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from customisable-log-analyser!');
+	});
+
+	context.subscriptions.push(disposable);
+
+	let runscriptsWithCurrent = vscode.commands.registerCommand('customisable-log-analyser.runscripts.current', () => {
+		logAnalyser.runWithTheCurrentFile();
+	});
+
+	context.subscriptions.push(runscriptsWithCurrent);
+
+	new TestView(context);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
